f0cb87517be1fb693565f186e21eaeba27e38ddf
AndriyVyrstabezp <andrijvirsta066@gmail.com>
2024-11-05 14:34:53 +0200

    add lab2

---
+++ lab2/scrip.py
@@ -0,0 +1,12 @@
+while True:
+    msg = input("Please input a msg: ")
+
+    if msg == "добрий день":
+        print("Добрий день")
+    elif msg == "слава україні":
+        print("Героям слава")
+    elif msg == "pa pa":
+        print("the end")
+        break
+    else:
+        print("Команда не розпізнана. Спробуйте ще раз.")
